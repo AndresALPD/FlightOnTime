000e130324b700969256b21b8fc84bd3cbfca32f
AndresALPD <andresalpizar03@gmail.com>
2026-01-06 20:01:19 -0600

    feat: agregar probabilidad de retraso a la respuesta de predicción

--- src/main/java/com/flightontime/dto/FlightDelayResponseDto.java
+++ src/main/java/com/flightontime/dto/FlightDelayResponseDto.java
@@ -8,4 +8,5 @@ public class FlightDelayResponseDto {
     private String airline_code;
     private Integer delay_prediction;
     private Boolean will_be_delayed;
+    private Double delay_probability;
 }
